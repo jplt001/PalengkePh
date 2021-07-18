61c34811a40cf725056f868324c224423438267e
JP Timcang <josephpatricktimcang@gmail.com>
2021-07-18 22:31:47 +0800

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View } from 'react-native';
+
+import useCachedResources from './hooks/useCachedResources';
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  if(!isLoadingComplete)
+  {
+    return null;
+  } else 
+  {
+    return (
+      <SafeAreaProvider>
+        
+      </SafeAreaProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
